a8eb3412c0c66a0ecf9915af9ade4c1257b249e7
Henry Nelson <Henry.Nelson89@gmail.com>
2020-04-10 08:52:39 -0700

    The data is now being displayed

--- client/src/Components/Players.js
+++ client/src/Components/Players.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 
 const PlayersList = props => {
-    console.log(props.player)
+    // console.log(props.player)
 
     return(
-        null
+        <div className= "players-list">
+            {props.player.map(person =>
+                <div key={person.id} className= "player-card">
+                    <h2 className="name" >{person.name}</h2>
+                    <p className= "information" >Country: {person.country}<br/> 
+                    Searches: {person.searches} </p>
+                </div>
+                )}
+        </div>
     )
 }
 
